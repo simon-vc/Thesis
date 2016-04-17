6398f62b55213126b3f1409570dd4e7cec833194
simon-vc <simonvancauter@gmail.com>
2016-04-17 16:23:40 +0200

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,7 @@
 echo "Install ruby"
 
 sudo apt-get update
+sudo apt-get -y install ruby-dev build-essential libssl-dev
 cd
 wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
 tar -xzvf ruby-2.2.3.tar.gz
