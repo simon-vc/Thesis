b3b4ef364ddc572ead7498deeaab67f6b1aedded
simon-vc <simonvancauter@gmail.com>
2016-04-18 09:38:42 +0200

    Update run_containers.sh

--- run_containers.sh
+++ run_containers.sh
@@ -1,13 +1,13 @@
 echo "Starting Docker containers"
 
-N=3
+N=5
 echo "Using default: $N containers"
 
 SECONDS=0
 for ((i=1 ; i<=$N ; i++))
 do
 	echo "Starting container$i"
-	docker run -d -v /etc/localtime:/etc/localtime:ro --name client$i simonvancauter/vfclient /bin/bash -c "cd client/ ./client client_config_http1.ini
+	docker run -d -v /etc/localtime:/etc/localtime:ro --name client$i simonvancauter/vfclient /bin/bash -c "cd client/ ; export LD_LIBRARY_PATH=bin:/usr/lib ; ./client client_config_http1.ini"
 	echo "Container $i started"
 	sleep $(( ( $RANDOM % 10 )  + 1 ))s
 	
