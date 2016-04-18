f745a176cb84397c336c72669631eaab901e6651
simon-vc <simonvancauter@gmail.com>
2016-04-18 17:03:58 +0200

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,12 +1,8 @@
 echo "Install ruby"
 
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo apt-get -y install ruby-dev build-essential libssl-dev
-cd
-wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
-tar -xzvf ruby-2.2.3.tar.gz
-cd ruby-2.2.3/
-./configure
-make
-echo "Installing ruby"
-sudo make install
+
+sudo apt-get -y install build-essential libssl-dev
+sudo apt-get -y install ruby2.2 ruby2.2-dev
