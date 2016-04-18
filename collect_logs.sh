e204dc2365e4c48d2441a2e6718ae41b1d0ba3fb
simon-vc <simonvancauter@gmail.com>
2016-04-18 11:39:09 +0200

    Update collect_logs.sh

--- collect_logs.sh
+++ collect_logs.sh
@@ -1,9 +1,10 @@
 echo "Getting logs"
 
 DATE=$(date +%Y%m%d_%H%M%S)
-mkdir ~/logs_$DATE
+mkdir ~/logs
+mkdir ~/logs/log_$DATE
 for i in $(docker ps -aq --format {{.Names}})
 do
 	echo $i
-	docker cp "$i:/var/www/html/log_client/" "./logs_$DATE/$i/"
+	docker cp "$i:/var/www/html/log_client/" "~/logs/logs_$DATE/$i/"
 done
