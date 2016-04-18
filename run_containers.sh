890977a2e4bf7f8d919d869d888a3164d189abb4
simon-vc <simonvancauter@gmail.com>
2016-04-18 11:02:57 +0200

    Update run_containers.sh

--- run_containers.sh
+++ run_containers.sh
@@ -1,15 +1,69 @@
-echo "Starting Docker containers"
+#Default values
+#Amount of containers
+N=10
+#Image name for the docker container
+IMAGE="simonvancauter/vfclient"
+#Random sleep time between container starts
+SLEEPTIME=10
 
-N=5
-echo "Using default: $N containers"
+#Read arguments
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -n|--number)
+    N="$2"
+	if [[ $2 -lt 1 ]]
+	then
+		echo "Amount of containers must be larger than 0"
+		echo "Using default: 10"
+		N=10
+	fi
+    shift
+    ;;
+    -i|--image)
+    IMAGE="$2"
+    shift
+    ;;
+    -s|--sleep)
+    SLEEPTIME="$2"
+		if [[ $SLEEPTIME -ge 0 ]]
+		then
+			echo "Random sleeptime set between 1 and $SLEEPTIME"
+		else
+			echo "Sleeptime must be positive"
+			SLEEPTIME=10
+		fi
+    shift
+    ;;
+    *)
+		echo "Argument not recognized"
+		# unknown option
+    ;;
+esac
+shift
+done
+
+
+echo "Starting Docker containers"
+echo "Amount: $N"
+echo "===================================="
 
 SECONDS=0
-for ((i=1 ; i<=$N ; i++))
+for ((i=1 ; it<=$N ; it++))
 do
 	echo "Starting container$i"
-	docker run -d -v /etc/localtime:/etc/localtime:ro --name client$i simonvancauter/vfclient /bin/bash -c "cd client/ ; export LD_LIBRARY_PATH=bin:/usr/lib ; ./client client_config_http1.ini"
-	echo "Container $i started"
-	sleep $(( ( $RANDOM % 10 )  + 1 ))s
-	
+	docker run -d -v /etc/localtime:/etc/localtime:ro --name client$i $IMAGE /bin/bash -c "cd client/ ; export LD_LIBRARY_PATH=bin:/usr/lib ; ./client client_config_http1.ini"
+	echo "Container $it started"
+	if [[ $i -ne $N ]]
+		then
+			if [[ $SLEEPTIME -gt 0 ]]
+			then
+				echo "Sleep"
+				sleep $(( ( $RANDOM % $SLEEPTIME )  + 1 ))s
+			fi
+	fi
 done
-echo "\n\n$SECONDS elapsed"
+echo "===================================="
+echo "$SECONDS elapsed"
