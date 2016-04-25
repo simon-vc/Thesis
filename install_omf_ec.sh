d2d525f187f7e9e8cde66c0afaf0ea6fed73c71b
simon-vc <simonvancauter@gmail.com>
2016-04-25 10:38:17 +0200

    Update install_omf_ec.sh

--- install_omf_ec.sh
+++ install_omf_ec.sh
@@ -1,1 +1,18 @@
-gem install omf_ec --no-ri --no-rdoc
+echo "Install ruby"
+sudo apt-get update
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz
+tar -xzvf ruby-2.2.3.tar.gz
+cd ruby-2.2.3/
+./configure
+make
+sudo make install
+
+echo "Installing omf_ec"
+sudo gem install omf_ec --no-ri --no-rdoc
+
+echo "Downloading omf script"
+#wget 
+
+echo "To run omf script using default values:"
+echo "omf_ec -u amqp://labwiki.test.atlantis.ugent.be vforce.rb"
