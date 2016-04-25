b664a490ab67d164c557c282ac0f0ccc5944643e
simon-vc <simonvancauter@gmail.com>
2016-04-25 10:32:30 +0200

    Update install_omf_rc.sh

--- install_omf_rc.sh
+++ install_omf_rc.sh
@@ -1,5 +1,11 @@
+echo "Install omf_rc"
 gem install omf_rc --no-ri --no-rdoc
 install_omf_rc -i -c
 
+echo "Configuring"
+echo "Default amqp server is amqp://labwiki.test.atlantis.ugent.be. This can be changed in /etc/omf_rc/config.yml"
 sudo wget https://raw.githubusercontent.com/simon-vc/Thesis/master/config.yml
 sudo cp config.yml /etc/omf_rc/config.yml
+
+echo "Starting omf_rc"
+sudo start omf_rc
