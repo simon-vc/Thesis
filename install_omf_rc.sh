0b2ea8eccf2263b468f299a67e37786461fe539a
simon-vc <simonvancauter@gmail.com>
2016-04-25 10:29:43 +0200

    Update install_omf_rc.sh

--- install_omf_rc.sh
+++ install_omf_rc.sh
@@ -1,2 +1,5 @@
 gem install omf_rc --no-ri --no-rdoc
 install_omf_rc -i -c
+
+sudo wget https://raw.githubusercontent.com/simon-vc/Thesis/master/config.yml
+sudo cp config.yml /etc/omf_rc/config.yml
