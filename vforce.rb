3220debf7edffff5aa451d4253338176471384fa
simon-vc <simonvancauter@gmail.com>
2016-04-28 17:13:14 +0200

    Update vforce.rb

--- vforce.rb
+++ vforce.rb
@@ -5,27 +5,20 @@
 defApplication('start_server') do |app|
    app.binary_path = "/bin/bash"
    app.description = "Starting the server"
-   app.defProperty('script', 'Script to run the server','', {:type => :string)
-end
-
-defApplication('create_containers') do |app|
-   app.binary_path = "/bin/bash"
-   app.description = "Creating containers"
-   app.defProperty('script', 'Script to create containers','', {:type => :string)
-   app.defProperty('amount', 'Amount of containers','-n', {:type => :integer)
+   app.defProperty('script', 'Script to run the server','', {:type => :string})
 end
 
 defApplication('run_containers') do |app|
    app.binary_path = "/bin/bash"
    app.description = "Running the containers"
-   app.defProperty('script', 'Script to run containers','', {:type => :string)
-   app.defProperty('amount', 'Amount of containers','-n', {:type => :integer)
+   app.defProperty('script', 'Script to run containers','', {:type => :string})
+   app.defProperty('amount', 'Amount of containers','-n', {:type => :integer})
 end
 
 defApplication('collect_logs') do |app|
    app.binary_path = "/bin/bash"
    app.description = "Collect the logs"
-   app.defProperty('script', 'Script to collect the logs','', {:type => :string)
+   app.defProperty('script', 'Script to collect the logs','', {:type => :string})
 end
 
 ###################################################
@@ -33,28 +26,20 @@ end
 ###################################################
 
 # Create a group by giving it a name and the DNS name of the resource you want to add to that group
-# ONLY LOWER CASE IS ALLOWED FOR THE DNS NAME
-defGroup("server","xxx") do |node|
+defGroup("server","server.largeset.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be") do |node|
   node.addApplication("start_server") do |app|
     app.setProperty('script', '/users/simonvc/code_thesis_simon/Server/run_server.sh')
   end
 end
 
-defGroup("client_create","yyy") do |node|
-  node.addApplication("create_containers") do |app|
-    app.setProperty('script', '/users/simonvc/create_containers.sh')
-    app.setProperty('amount', 10)
-  end
-end
-
-defGroup("client_run","yyy") do |node|
+defGroup("client_run","client1.largeset.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be") do |node|
   node.addApplication("run_containers") do |app|
     app.setProperty('script', '/users/simonvc/run_containers.sh')
-    app.setProperty('amount', 10)
+    app.setProperty('amount', 1)
   end
 end
 
-defGroup("client_collect","yyy") do |node|
+defGroup("client_collect","client1.largeset.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be") do |node|
   node.addApplication("collect_logs") do |app|
     app.setProperty('script', '/users/simonvc/collect_logs.sh')
   end
@@ -65,19 +50,18 @@ end
 onEvent(:ALL_UP) do |event|
   # print some info to the STDOUT of the OMF EC
   info "Starting experiment..."
-  group("client_create").startApplications
-  after 60 do
+  after 5 do
     info "Starting server..."
     group("server").startApplications
   end
-  after 70 do
+  after 15 do
     info "Starting client..."
     group("client_run").startApplications
   end
-  after 1200 do
-	group("client_collect").startApplications
+  after 900 do
+    group("client_collect").startApplications
   end
-  after 1260 do
+  after 1000 do
     info "All applications are stopped now..."
     allGroups.stopApplications
     Experiment.done
