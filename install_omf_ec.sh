264563d80b043b359a6a6985493b9ad54797f2f4
simon-vc <simonvancauter@gmail.com>
2016-04-28 08:43:29 +0200

    Update install_omf_ec.sh

--- install_omf_ec.sh
+++ install_omf_ec.sh
@@ -12,7 +12,7 @@ echo "Installing omf_ec"
 sudo gem install omf_ec --no-ri --no-rdoc
 
 echo "Downloading omf script"
-#wget 
+wget https://raw.githubusercontent.com/simon-vc/Thesis/master/vforce.rb 
 
 echo "To run omf script using default values:"
 echo "omf_ec -u amqp://labwiki.test.atlantis.ugent.be vforce.rb"
