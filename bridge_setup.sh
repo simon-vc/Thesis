d29ded53189a19156a4a1d59604a2eb62563caff
simon-vc <simonvancauter@gmail.com>
2016-04-28 11:09:48 +0200

    Update bridge_setup.sh

--- bridge_setup.sh
+++ bridge_setup.sh
@@ -1,7 +1,8 @@
 echo "Bridge setup"
 
-brctl addbr br0
-brctl addif br0 eth1 eth4
-ifconfig eth1 0.0.0.0
-ifconfig eth4 0.0.0.0
-ifconfig br0 10.0.0.10 netmask 255.255.255.0 up
+sudo apt-get install -y bridge-utils
+sudo brctl addbr br0
+sudo brctl addif br0 eth1 eth4
+sudo ifconfig eth1 0.0.0.0
+sudo ifconfig eth4 0.0.0.0
+sudo ifconfig br0 10.0.0.10 netmask 255.255.255.0 up
