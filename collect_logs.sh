efc4cbbc31ddda5c1d1a59f481c25d29987e26e3
simon-vc <simonvancauter@gmail.com>
2016-04-29 17:57:25 +0200

    Update collect_logs.sh

--- collect_logs.sh
+++ collect_logs.sh
@@ -1,10 +1,26 @@
 echo "Getting logs"
 
-DATE=$(date +%Y%m%d_%H%M%S)
-mkdir ~/logs
-mkdir ~/logs/log_$DATE
+FOUND=false
+ITERATIE=0
+DC=~/logs
+while [ $FOUND != true ]
+do
+	if [ -d "$DC/log$ITERATIE" ]
+	then
+		((ITERATIE+=1))
+	else
+		FOUND=true
+	fi
+done
+echo "Iteratie $ITERATIE"
+if [ ! -d "$DC" ]
+then
+	mkdir ~/logs/
+fi
+mkdir ~/logs/log$ITERATIE/
+
 for i in $(docker ps -aq --format {{.Names}})
 do
 	echo $i
-	docker cp $i:/var/www/html/log_client/ ~/logs/log_$DATE/$i/
+	docker cp $i:/var/www/html/log_client/ ~/logs/log$ITERATIE/$i/
 done
