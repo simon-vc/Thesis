0c57989daf7281e2ecf89f60d497fc853edbb215
simon-vc <simonvancauter@gmail.com>
2016-04-30 22:42:21 +0200

    Create collect.rb

---
+++ collect.rb
@@ -0,0 +1,36 @@
+###################################################
+################### APP DEF #######################
+###################################################
+
+defApplication('collect') do |collect|
+   collect.binary_path = '/bin/bash'
+   collect.description = 'Collect the logs'
+   collect.defProperty('script', 'Script to collect the logs','-c', {:type => :string})
+end
+
+###################################################
+################### EXP DESCRIPTION ###############
+###################################################
+
+# Create a group by giving it a name and the DNS name of the resource you want to add to that group
+defGroup('clientgroup','client1.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be') do |clientnode|
+  clientnode.addApplication('collect') do |collect|
+    collect.setProperty('script', 'export HOME=/users/simonvc/ ; cd /users/simonvc/ ; bash collect_logs.sh')
+  end
+end
+
+# wait for the event ALL_UP before starting the experiment
+# (all resources are booted and OMF RC is started)
+onEvent(:ALL_UP) do |event|
+  # print some info to the STDOUT of the OMF EC
+  info "Starting experiment..."
+  after 10 do
+    info "Collecting logs"
+    group('clientgroup').startApplications
+  end
+  after 120 do
+    info "All applications are stopped now..."
+    allGroups.stopApplications
+    Experiment.done
+  end
+end
