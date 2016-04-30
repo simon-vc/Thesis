a7a88f96cb3c3bf604efca60a7de5436d27c8046
simon-vc <simonvancauter@gmail.com>
2016-04-30 22:05:30 +0200

    Update vforce.rb

--- vforce.rb
+++ vforce.rb
@@ -2,20 +2,20 @@
 ################### APP DEF #######################
 ###################################################
 
-defApplication('start_server', 'start_server') do |server|
+defApplication('server', 'serverStart') do |server|
    server.binary_path = '/bin/bash'
    server.description = 'Starting the server'
    server.defProperty('script', 'Script to run the server','-c', {:type => :string})
 end
 
-defApplication('run', 'run') do |run|
+defApplication('run', 'runClients') do |run|
    run.binary_path = '/bin/bash'
    run.description = 'Running the containers'
    run.defProperty('script', 'Script to run containers with arguments','', {:type => :string})
    run.defProperty('amount', 'Amount of containers', '-n', {:type => :integer})
 end
 
-defApplication('collect', 'collect') do |collect|
+defApplication('collect', 'collectLogs') do |collect|
    collect.binary_path = '/bin/bash'
    collect.description = 'Collect the logs'
    collect.defProperty('script', 'Script to collect the logs','-c', {:type => :string})
@@ -26,19 +26,19 @@ end
 ###################################################
 
 # Create a group by giving it a name and the DNS name of the resource you want to add to that group
-defGroup('server','server.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be') do |server|
-  server.addApplication('start_server') do |server|
-    server.setProperty('script', 'export HOME=/users/simonvc/ ; cd /users/simonvc/code_thesis_simon/Server/ ; bash run_server.sh')
+defGroup('servergroup','server.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be') do |servernode|
+  servernode.addApplication('serverStart') do |serverapp|
+    serverapp.setProperty('script', 'export HOME=/users/simonvc/ ; cd /users/simonvc/code_thesis_simon/Server/ ; bash run_server.sh')
   end
 end
 
-defGroup('client','client1.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be') do |client|
-  client.addApplication('run') do |run|
-    run.setProperty('script', '/users/simonvc/run_containers.sh')
-    run.setProperty('amount', 1)
+defGroup('clientgroup','client1.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be') do |clientnode|
+  clientnode.addApplication('runClients') do |runapp|
+    runapp.setProperty('script', '/users/simonvc/run_containers.sh')
+    runapp.setProperty('amount', 1)
   end
-  client.addApplication('collect') do |collect|
-    collect.setProperty('script', 'export HOME=/users/simonvc/ ; cd /users/simonvc/ ; bash collect_logs.sh')
+  clientnode.addApplication('collectLogs') do |collectapp|
+    collectapp.setProperty('script', 'export HOME=/users/simonvc/ ; cd /users/simonvc/ ; bash collect_logs.sh')
   end
 end
 
@@ -53,11 +53,11 @@ onEvent(:ALL_UP) do |event|
   end
   after 40 do
     info "Starting client..."
-    group('client').startApplication('run')
+    group('client').startApplication('runClients')
   end
   after 200 do
     info "Collecting logs"
-    group('client').startApplication('collect')
+    group('client').startApplication('collectLogs')
   end
   after 220 do
     info "All applications are stopped now..."
