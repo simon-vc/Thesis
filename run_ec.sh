84d5d6ef19177c1695003bf8258004353e0d6327
simon-vc <simonvancauter@gmail.com>
2016-05-01 15:16:07 +0200

    Update run_ec.sh

--- run_ec.sh
+++ run_ec.sh
@@ -1,2 +1,2 @@
-omf_ec -u amqp://labwiki.test.atlantis.ugent.be vforce.rb
-omf_ec -u amqp://labwiki.test.atlantis.ugent.be collect.rb
+omf_ec -u amqp://controller.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be vforce.rb
+omf_ec -u amqp://controller.full2.wall2-ilabt-iminds-be.wall2.ilabt.iminds.be collect.rb
