660bafde19f01968859ec42af4dba0459172168c
simon-vc <simonvancauter@gmail.com>
2016-05-02 17:44:35 +0200

    Update install_omf_ec.sh

--- install_omf_ec.sh
+++ install_omf_ec.sh
@@ -22,6 +22,7 @@ sudo apt-get update
 sudo apt-get -y install rabbitmq-server
 sudo wget https://raw.githubusercontent.com/simon-vc/Thesis/master/rabbitmq.config
 sudo cp rabbitmq.config /etc/rabbitmq/rabbitmq.config
+sudo service rabbitmq-server restart
 
 echo "To run omf script using default values:"
 echo "bash run_ec.sh"
