2a9c9de6f3c9588931a03af8072891acd1f221de
simon-vc <simonvancauter@gmail.com>
2016-05-02 17:59:58 +0200

    Update bridge_setup.sh

--- bridge_setup.sh
+++ bridge_setup.sh
@@ -4,12 +4,9 @@ if [ $# -ne 2 ]
     exit
 fi
 
-$IF1=$1
-$IF2=$2
-
 echo "Bridge setup"
 sudo brctl addbr br0
-sudo brctl addif br0 $IF1 $IF2
-sudo ifconfig $IF1 0.0.0.0
-sudo ifconfig $IF2 0.0.0.0
+sudo brctl addif br0 $1 $2
+sudo ifconfig $1 0.0.0.0
+sudo ifconfig $2 0.0.0.0
 sudo ifconfig br0 10.0.0.10 netmask 255.255.255.0 up
