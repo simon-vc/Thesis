7f331ad82f30ef0d4d3cdd8d8e1cdd1c4b934d87
simon-vc <simonvancauter@gmail.com>
2016-05-02 12:19:04 +0200

    Update bridge_setup.sh

--- bridge_setup.sh
+++ bridge_setup.sh
@@ -1,7 +1,15 @@
-echo "Bridge setup"
+if [ $# -ne 2 ]
+  then
+    echo "Use $0 interface1 interface2"
+    exit
+fi
 
+$IF1=$1
+$IF2=$2
+
+echo "Bridge setup"
 sudo brctl addbr br0
-sudo brctl addif br0 eth1 eth4
-sudo ifconfig eth1 0.0.0.0
-sudo ifconfig eth4 0.0.0.0
+sudo brctl addif br0 $IF1 $IF2
+sudo ifconfig $IF1 0.0.0.0
+sudo ifconfig $IF2 0.0.0.0
 sudo ifconfig br0 10.0.0.10 netmask 255.255.255.0 up
