b2a90edd50f24135824e103e73ccbc7190ead2af
simon-vc <simonvancauter@gmail.com>
2016-05-03 09:03:21 +0200

    Update run_containers.sh

--- run_containers.sh
+++ run_containers.sh
@@ -39,6 +39,12 @@ esac
 shift
 done
 
+echo "Start logging cpu, mem and disk usage"
+echo "===================================="
+sudo nohup sar 10 72 >cpu.log &
+sudo nohup sar -r 10 72 >mem.log &
+sudo nohup sar -F 10 72 >disk.log &
+
 
 echo "Starting Docker containers"
 echo "Amount: $N"
